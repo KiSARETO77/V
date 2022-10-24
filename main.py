ba5f12e325bb95c51f54eaeb725f8d409c62eb33
KiSARETO77 <106799027+KiSARETO77@users.noreply.github.com>
2022-10-24 16:38:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ for A in W(open("accounts.json")):
 	E,P,D=A["email"],A["password"],A["device"]
 	Ç=amino.Client(deviceId=D,proxies={"https":"http://dimatjasko10:KWJYs68q@185.112.13.43:2831"})
 	Ç.login(E,P)
-	L=Ç.get_from_code("http://aminoapps.com/c/anime-empire-1")
-	O="59e378d4-1897-4e21-ab42-63861568727f"
+	L=Ç.get_from_code("http://aminoapps.com/c/Dragon-ball-empire")
+	O="0029e51c-6490-480c-9205-b70e262a16d1"
 	C=3434136
 	print(f"Login To {E}")
 	try:
